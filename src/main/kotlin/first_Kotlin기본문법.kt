e722867f277df1a72fda603cdb1f3f10af607cbb
iuyt <itsyksj@gmail.com>
2023-10-06 20:44:22 +0900

    Kotlin 기본문법(null 추가)

--- src/main/kotlin/first_Kotlin기본문법.kt
+++ src/main/kotlin/first_Kotlin기본문법.kt
@@ -2,7 +2,8 @@ import java.util.*
 import kotlin.random.Random
 
 //기본적으로 Kotlin은 맨 마지막에 ; 작성안함
-//1. 출력구문
+
+//출력구문
 fun 출력구문(){
     println("줄 바꿈 있는 출력")
     print("줄 바꿈 없는 출력형")
@@ -10,8 +11,7 @@ fun 출력구문(){
 }
 
 
-
-//2-1. var(변수) - 다른 값으로 재대입 가능
+//var(변수) - 다른 값으로 재대입 가능, 퍼런스 타입(첫글자 대문자) = 명시적 타입지정
 //변수선언 시 별도의 타입지정 필요없음 -> 코틀린은 타입추론 가능
 fun 변수1(){
     var var_i1 = 10
@@ -21,7 +21,7 @@ fun 변수1(){
     var_i1 = 15
 }
 
-//2-2. var - 퍼런스 타입(첫글자 대문자) = 명시적 타입지정
+
 fun 변수2(){
     var var_i2 : Int = 10                   //Int = 레퍼런스 타입(명시적 타입지정)
     var var_l2 : Long = 20L                 //Long = 레퍼런스 타입(명시적 타입지정)
@@ -30,22 +30,22 @@ fun 변수2(){
 }
 
 
-
-//3-1. val(상수) - 다른 값으로 재대입 불가능(컴파일 시 에러발생)
+//val(상수) - 다른 값으로 재대입 불가능(컴파일 시 에러발생)
 fun 상수1(){
     val val_num1 = 10
     //val_num1 = 70 -> 컴파일 시 에러발생
 }
 
-//3-2. Top Level val(최상위 상수) - 클래스 바깥에서 선언가능
+
+//Top Level val(최상위 상수) - 클래스 바깥에서 선언가능
 val val_num2 = 20
 
-//3-3. compile-time constants(컴파일 타임 상수) - 메인보다 우선 컴파일 되며 성능우위 가짐
+
+// compile-time constants(컴파일 타임 상수) - 메인보다 우선 컴파일 되며 성능우위 가짐
 const val val_num3 = 30     //클래스 안에서 선언 시 에러발생
 
 
-
-//4. Type Casting(형변환) - 타입이 다를 경우 to[Type]() 실시
+//Type Casting(형변환) - 타입이 다를 경우 to[Type]() 실시
 fun 형변환(){
     var var_i3 = 10
     var var_l3 = 20L
@@ -58,8 +58,7 @@ fun 형변환(){
 }
 
 
-
-//5. String(문자형)
+//String(문자형)
 fun 문자형(){
     var var_name4 = "Kim SuJin"
 
@@ -76,8 +75,45 @@ fun 문자형(){
 }
 
 
+//Null Safety
 
-//6. max, min
+//null 값 넣기 - var 변수명 : Type? = null
+//null 안 들어감 - var 변수명 : Type = null
+fun Null(){
+    var var_byte : Byte? = null
+    var var_short : Short? = null
+    var var_int : Int? = null
+    var var_long : Long? = null
+
+    var var_char : Char? = null
+    var var_string : String? = null
+}
+
+
+//Null 체크 - 타입 동일여부 확인
+var null_check1 : String? = null
+var null_check2 : String? = " "
+fun Null_체크1(){
+    if(null_check2 != null_check1){
+        null_check1 = null_check2
+    }
+}
+
+// 강제적으로 Null 값 변환 - 위의 코드실행 결과는 같지만 추천안함
+fun Null_체크2(){
+    null_check1 = null_check2!!
+}
+
+
+//null이 아닌경우 값을 실행시켜라
+fun Null_체크3(){
+    null_check1.let {
+        null_check2 = null_check1
+    }
+}
+
+
+//max, min
 fun 숫자_구하기(){
     var var_min = 10
     var var_max = 20
@@ -92,16 +128,14 @@ fun 숫자_구하기(){
 }
 
 
-
-//7. Random(Kotlin 내장함수 - import kotlin.random.Random)
+//Random(Kotlin 내장함수 - import kotlin.random.Random)
 fun 랜덤값_구하기(){
     var randomNumber1 = Random.nextInt(0, 100)          //0 ~ 99 랜덤값 출력
     var randomNumber2 = Random.nextDouble(0.0, 1.0)     //0.0 ~ 9.9 랜덤값 출력
 }
 
 
-
-//8. 키보드 입력받기(Java 내장함수 - import java.util)
+//키보드 입력받기(Java 내장함수 - import java.util)
 //Kotlin에서 사용불가 키워드는 앞뒤로 백틱(`)을 쓰면 사용가능
 fun 키보드_입력받기(){
     var var_read = Scanner(System.`in`)
